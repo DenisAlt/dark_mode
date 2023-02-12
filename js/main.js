5c0957cea0fd8ffc2310d719902a39178caecf60
Denis Altukhov <alt-den87@yandex.ru>
2023-02-12 12:50:40 +0300

    localStorage

--- js/main.js
+++ js/main.js
@@ -2,22 +2,33 @@ const button = document.querySelector('.dark-mode-btn');
 const switchBtn = document.querySelector('.dark-mode-btn__switch');
 const switchStyle = getComputedStyle(switchBtn);
 
+
 button.addEventListener('click', modeOn)
 
 function modeOn() {
   switchBtn.classList.toggle('dark-mode-btn__switch--on');
   const swithPosition = parseInt(switchStyle.left);
-  if (swithPosition === 77) {
+  if (swithPosition == 77) {
+    localStorage.setItem('darkMode', 'light')
     setTimeout(() => {
       document.body.style.backgroundColor = '#fff'
       button.classList.remove('dark-mode-btn--on');
       switchBtn.style.backgroundColor = '#e0c129';
     }, 250)
   } else {
+    localStorage.setItem('darkMode', 'dark')
     setTimeout(() => {
       document.body.style.backgroundColor = '#202020';
       button.classList.add('dark-mode-btn--on');
       switchBtn.style.backgroundColor = '#131313';
     }, 250)
   };
+
 }
+
+if (localStorage.getItem('darkMode') === 'dark') {
+  switchBtn.classList.add('dark-mode-btn__switch--on');
+  document.body.style.backgroundColor = '#202020';
+  button.classList.add('dark-mode-btn--on');
+  switchBtn.style.backgroundColor = '#131313';
+} 
